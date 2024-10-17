3c7046516d0f3a874f794d321f41f47acd178421
Anastasiia Bulatkina <anstbulatkina@gmail.com>
2024-10-17 10:58:37 +0200

    Refactor app.py and templates/auth/login.html, add blueprints and login routes, and initialize extensions

--- app.py
+++ app.py
@@ -11,15 +11,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://medcore:medcorepassword@localhost:3306/medcore_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['REMEMBER_COOKIE_DURATION'] = timedelta(days=7)
+app.secret_key = 'mysecretkey'
 
-appointments = [
-    {'time': '10:00 AM', 'patient_name': 'John Doe', 'arrived': False, 'id': 1},
-    {'time': '11:00 AM', 'patient_name': 'Jane Smith', 'arrived': False, 'id': 2}
-]
-
+# Initialize extensions
 db.init_app(app)
 migrate = Migrate(app, db)
-app.secret_key = 'mysecretkey'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -29,12 +25,16 @@ login_manager.login_view = 'auth.login'
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Blueprints
+# Register blueprints
 app.register_blueprint(auth_bp, url_prefix='/auth')
 
-print(app.url_map)
+# Sample data for appointments
+appointments = [
+    {'time': '10:00 AM', 'patient_name': 'John Doe', 'arrived': False, 'id': 1},
+    {'time': '11:00 AM', 'patient_name': 'Jane Smith', 'arrived': False, 'id': 2}
+]
 
-
+# Routes
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -48,10 +48,12 @@ def contact():
     return render_template('contact.html')
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
     return render_template('/doctor/dashboard.html', appointments=appointments)
 
 @app.route('/mark_arrived/<int:appointment_id>', methods=['POST'])
+@login_required
 def mark_arrived(appointment_id):
     # Find the appointment by ID and mark it as arrived
     for appointment in appointments:
@@ -59,7 +61,6 @@ def mark_arrived(appointment_id):
             appointment['arrived'] = True
             break
     return redirect(url_for('dashboard'))
-
 
 @app.route('/test-db')
 def test_db():
