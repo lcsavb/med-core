fb33dbdaa6b73b38261971e9fcc349da94e21c4e
Lucas <lcsavb@gmail.com>
2024-11-15 17:56:38 +0100

    first unit tests

--- app.py
+++ app.py
@@ -20,11 +20,32 @@ from rate_limit import limiter
 
 configure_logging()
 
-# Flask application initialization
-app = Flask(__name__)
-api = Api(app)
-app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")  # Change this!
-jwt = JWTManager(app)
+def create_app():
+    # Flask application initialization
+    app = Flask(__name__)
+    api = Api(app)
+    app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")  # Change this!
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")  # Change this!
+    jwt = JWTManager(app)
+
+    # Resources
+    api.add_resource(LoginResource, '/auth/login')
+    api.add_resource(RegisterResource, '/auth/register')
+    api.add_resource(LogoutResource, '/auth/logout')
+    api.add_resource(StatusResource, '/auth/status')
+    api.add_resource(ProtectedResource, '/auth/protected')
+    api.add_resource(Verify2FAResource, '/auth/verify-2fa')
+    api.add_resource(ClinicsResource, '/api/clinics/<int:clinic_id>')
+    api.add_resource(ProfessionalsResource, '/api/clinics/<int:clinic_id>/doctors')
+    api.add_resource(ProfessionalByIdResource, '/api/clinics/<int:clinic_id>/doctors/<int:healthcare_professional_id>')
+    api.add_resource(PatientsResource, '/api/clinics/<int:clinic_id>/patients')
+    api.add_resource(PatientsByDoctorsResource, '/api/clinics/<int:clinic_id>/doctors/<int:healthcare_professional_id>/patients')
+    api.add_resource(DoctorScheduleResource, '/api/clinics/<int:clinic_id>/doctors/<int:healthcare_professional_id>/schedules')
+    api.add_resource(AppointmentsResource, '/api/appointments/')
+
+    return app
+
+app = create_app()
 
 # Rate limiter initialization and configuration to prevent abuse and brute force attacks
 limiter.init_app(app)
@@ -37,20 +58,6 @@ def rate_limit_handler(e):
 # Secret key for session management
 app.secret_key = os.environ.get("SECRET_KEY", "default-secret-key")
 
-# Resources
-api.add_resource(LoginResource, '/auth/login')
-api.add_resource(RegisterResource, '/auth/register')
-api.add_resource(LogoutResource, '/auth/logout')
-api.add_resource(StatusResource, '/auth/status')
-api.add_resource(ProtectedResource, '/auth/protected')
-api.add_resource(Verify2FAResource, '/auth/verify-2fa')
-api.add_resource(ClinicsResource, '/api/clinics/<int:clinic_id>')
-api.add_resource(ProfessionalsResource, '/api/clinics/<int:clinic_id>/doctors')
-api.add_resource(ProfessionalByIdResource, '/api/clinics/<int:clinic_id>/doctors/<int:healthcare_professional_id>')
-api.add_resource(PatientsResource, '/api/clinics/<int:clinic_id>/patients')
-api.add_resource(PatientsByDoctorsResource, '/api/clinics/<int:clinic_id>/doctors/<int:healthcare_professional_id>/patients')
-api.add_resource(DoctorScheduleResource, '/api/clinics/<int:clinic_id>/doctors/<int:healthcare_professional_id>/schedules')
-api.add_resource(AppointmentsResource, '/api/appointments/')
 
 
 if __name__ == '__main__':
